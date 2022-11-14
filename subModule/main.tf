3c30936bbf31eee6fa898df7e9631bf802e253ac
Yaroslav Subota <111CRB111@gmail.com>
2022-11-14 12:44:14 +0200

    Update main.tf

--- subModule/main.tf
+++ subModule/main.tf
@@ -1,4 +1,8 @@
 module "s3-bucket_notification" {
 	source  = "0f5e261347aa.test-env.scalr.com/env-svrcnchebt61e30/s3-bucket/aws//modules/notification"
 	version = "1.0.2"
+	bucket = "crb-test-bucket-123123123"
+	tags = {
+	    "owner" = "y.subota@scalr.com"
+	}
 }
